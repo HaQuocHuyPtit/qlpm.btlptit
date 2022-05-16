559515c924070944bc33e3fff9af99bc80d1424f
= <=>
2022-05-16 10:59:11 +0700

    first commit

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= '{field} không được để trống !';
+$lang['form_validation_isset']			= 'The {field} field must have a value.';
+$lang['form_validation_valid_email']		= '{field} không đúng định dạng';
+$lang['form_validation_min_length']		= '{field} dài tối thiểu {param} kí tự';
+$lang['form_validation_max_length']		= '{field} tối đa chỉ được {param} kí tự.';
+$lang['form_validation_valid_emails']		= 'Email đã được sử dụng, vui lòng nhập email khác';
+$lang['form_validation_matches']		= '{field} không khớp.';
+$lang['form_validation_is_unique'] 		= '{field} đã là thành viên, vui lòng đăng nhập hoặc nhập {field} khác !';
+$lang['form_validation_regex_match']		= ' {field} không đúng định dạng.';
+$lang['form_validation_numeric']		= '{field} không đúng định dạng số.';
+
+
